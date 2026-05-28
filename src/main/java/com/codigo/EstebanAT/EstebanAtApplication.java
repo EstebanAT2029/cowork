471bbf0fdb0423c417f3c62d50587954f3a98770
Esteban <esteban2029@gmail.com>
2026-05-27 19:42:01 -0500

    Subiendo Proyecto del Examen Modulo 3 Java FullStack. Aturor: Esteban Apaza Ticona

---
+++ src/main/java/com/codigo/EstebanAT/EstebanAtApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.EstebanAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstebanAtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstebanAtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigo/EstebanAT/EstebanAtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.EstebanAT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstebanAtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
